99ca884745ef78c297082b11aadfa0daa60340fb
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-20 00:26:51 -0300

    update gerenciaropcoes_ticket.js

--- Comandos/Tickets/config/gerenciaropcoes_ticket.js
+++ Comandos/Tickets/config/gerenciaropcoes_ticket.js
@@ -8,19 +8,15 @@ module.exports = {
   {newEmbed: {title:Opções Criadas} {color:$getGuildVar[color_ticket]} {footer:$guildname | Todos os Direitos Reservados}{author:$guildname | Atendimentos}{thumbnail:$guildicon}
     $if[$getGuildVar[titulo1]!=]
   {field:1. $getGuildVar[emoji1] | $getGuildVar[titulo1]:$getGuildVar[desc1]}
-  $if[$getGuildVar[titulo2]!=]
+  $endif $if[$getGuildVar[titulo2]!=]
   {field:2. $getGuildVar[emoji2] | $getGuildVar[titulo2]:$getGuildVar[desc2]}
-  $if[$getGuildVar[titulo3]!=]
+  $endif $if[$getGuildVar[titulo3]!=]
   {field:3. $getGuildVar[emoji3] | $getGuildVar[titulo3]:$getGuildVar[desc3]}
-  $if[$getGuildVar[titulo4]!=]
+  $endif $if[$getGuildVar[titulo4]!=]
   {field:4. $getGuildVar[emoji4] | $getGuildVar[titulo4]:$getGuildVar[desc4]}
-  $if[$getGuildVar[titulo5]!=]
+  $endif $if[$getGuildVar[titulo5]!=]
   {field:5. $getGuildVar[emoji5] | $getGuildVar[titulo5]:$getGuildVar[desc5]}
- $endif
- $endif
- $endif
- $endif
- $endif}
+ $endif }
   {actionRow:{button:Adicionar Opção:success:addopcao_ticket:$if[$getGuildVar[titulo5]==$getvar[titulo5]]true$endif:}{button:Remover Opção:danger:remopcao_ticket::}{button:Voltar para Menu:secondary:voltarconfig::🔙}}]
   
   `
