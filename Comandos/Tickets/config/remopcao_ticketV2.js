41109ed3aa21bf05086a7dcf7e6f135dc25ade09
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-20 00:08:07 -0300

    update remopcao_ticketV2.js

--- Comandos/Tickets/config/remopcao_ticketV2.js
+++ Comandos/Tickets/config/remopcao_ticketV2.js
@@ -11,12 +11,7 @@ module.exports = {
  $let[menu2;$get[menu]]
  $let[menu;{actionRow:{selectMenu:rmopcao_ticket:Escolha uma Opção para Removê-la.:1:1:false:$if[$getguildvar[emoji1]!=n;{stringInput:$getguildvar[titulo1]:opcao1:$getguildvar[desc1]::$getguildvar[emoji1]}]$if[$getguildvar[emoji2]!=n;:{stringInput:$getguildvar[titulo2]:opcao2:$getguildvar[desc2]::$getguildvar[emoji2]}]$if[$getguildvar[emoji3]!=n;:{stringInput:$getguildvar[titulo3]:opcao3:$getguildvar[desc3]::$getguildvar[emoji3]}]$if[$getguildvar[emoji4]!=n;:{stringInput:$getguildvar[titulo4]:opcao4:$getguildvar[desc4]::$getguildvar[emoji4]}]$if[$getguildvar[emoji5]!=n;:{stringInput:$getguildvar[titulo5]:opcao5:$getguildvar[desc5]::$getguildvar[emoji5]}]}}]
  
- $switch[$getSelectMenuValues[all;,];
- {case:opcao1:$setGuildVar[titulo1;$getGuildVar[titulo2]] $setGuildVar[titulo2;$getGuildVar[titulo3]] $setGuildVar[titulo3;$getGuildVar[titulo4]] $setGuildVar[titulo4;$getGuildVar[titulo5]] $resetguildvar[titulo5] $setGuildVar[desc1;$getGuildVar[desc2]] $setGuildVar[desc2;$getGuildVar[desc3]] $setGuildVar[desc3;$getGuildVar[desc4]] $setGuildVar[desc4;$getGuildVar[desc5]] $resetguildvar[desc5] $setGuildVar[emoji1;$getGuildVar[emoji2]] $setGuildVar[emoji2;$getGuildVar[emoji3]] $setGuildVar[emoji3;$getGuildVar[emoji4]] $setGuildVar[emoji4;$getGuildVar[emoji5]] $resetguildvar[emoji5]
- } {case:opcao2:$setGuildVar[titulo2;$getGuildVar[titulo3]] $setGuildVar[titulo3;$getGuildVar[titulo4]] $setGuildVar[titulo4;$getGuildVar[titulo5]] $resetguildvar[titulo5] $setGuildVar[desc2;$getGuildVar[desc3]] $setGuildVar[desc3;$getGuildVar[desc4]] $setGuildVar[desc4;$getGuildVar[desc5]] $resetguildvar[desc5] $setGuildVar[emoji2;$getGuildVar[emoji3]] $setGuildVar[emoji3;$getGuildVar[emoji4]] $setGuildVar[emoji4;$getGuildVar[emoji5]] $resetguildvar[emoji5]
- } {case:opcao3:$setGuildVar[titulo3;$getGuildVar[titulo4]] $setGuildVar[titulo4;$getGuildVar[titulo5]] $resetguildvar[titulo5] $setGuildVar[desc3;$getGuildVar[desc4]] $setGuildVar[desc4;$getGuildVar[desc5]] $resetguildvar[desc5] $setGuildVar[emoji3;$getGuildVar[emoji4]] $setGuildVar[emoji4;$getGuildVar[emoji5]] $resetguildvar[emoji5]
- } {case:opcao4:$setGuildVar[titulo4;$getGuildVar[titulo5]] $resetguildvar[titulo5] $setGuildVar[desc4;$getGuildVar[desc5]] $resetguildvar[desc5] $setGuildVar[emoji4;$getGuildVar[emoji5]] $resetguildvar[emoji5]
- } {case:opcao5:}]
+
  
  $resetguildvar[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$nonescape[$getSelectMenuValues[all;,]];opcao1;titulo1];opcao2;titulo2];opcao3;titulo3];opcao4;titulo4];opcao5;titulo5]]
  $resetguildvar[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$nonescape[$getSelectMenuValues[all;,]];opcao1;emoji1];opcao2;emoji2];opcao3;emoji3];opcao4;emoji4];opcao5;emoji5]]
