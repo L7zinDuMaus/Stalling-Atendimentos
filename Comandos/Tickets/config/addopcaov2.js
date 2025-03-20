f56ce358ecd8c0a0a176992b14b9560dc12948a1
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-20 00:50:56 -0300

    update addopcaov2.js

--- Comandos/Tickets/config/addopcaov2.js
+++ Comandos/Tickets/config/addopcaov2.js
@@ -4,24 +4,22 @@ module.exports = {
   prototype: "modal",
   $if: "old",
   code: `
+  $if[1==1]
   $interactionupdate[**Utilize os Botões abaixo para adicionar e remover opções do Menu de Seleção do Painel de Tickets.** 
   {newEmbed: {title:Opções Criadas} {color:$getGuildVar[color_ticket]} {footer:$guildname | Todos os Direitos Reservados}{author:$guildname | Atendimentos}{thumbnail:$guildicon}
     $if[$getGuildVar[titulo1]!=]
   {field:1. $getGuildVar[emoji1] | $getGuildVar[titulo1]:$getGuildVar[desc1]}
-  $if[$getGuildVar[titulo2]!=]
+  $endif $if[$getGuildVar[titulo2]!=]
   {field:2. $getGuildVar[emoji2] | $getGuildVar[titulo2]:$getGuildVar[desc2]}
-  $if[$getGuildVar[titulo3]!=]
+  $endif $if[$getGuildVar[titulo3]!=]
   {field:3. $getGuildVar[emoji3] | $getGuildVar[titulo3]:$getGuildVar[desc3]}
-  $if[$getGuildVar[titulo4]!=]
+  $endif $if[$getGuildVar[titulo4]!=]
   {field:4. $getGuildVar[emoji4] | $getGuildVar[titulo4]:$getGuildVar[desc4]}
-  $if[$getGuildVar[titulo5]!=]
+  $endif $if[$getGuildVar[titulo5]!=]
   {field:5. $getGuildVar[emoji5] | $getGuildVar[titulo5]:$getGuildVar[desc5]}
- $endif
- $endif
- $endif
- $endif
- $endif}
+ $endif }
   {actionRow:{button:Adicionar Opção:success:addopcao_ticket:$if[$getGuildVar[titulo5]==$getvar[titulo5]]true$endif:}{button:Remover Opção:danger:remopcao_ticket::}{button:Voltar para Menu:secondary:voltarconfig::🔙}}]
+  $endif
 
   $if[$getGuildVar[titulo1]==]
   $setGuildVar[titulo1;$get[titulo]] 
@@ -50,8 +48,8 @@ module.exports = {
   $setGuildVar[titulo5;$get[titulo]] 
   $setGuildVar[desc5;$get[desc]]
   $setGuildVar[emoji5;$get[emoji]]
- $endelseif
- $endif
+  $endelseif
+  $endif
 
 
   $let[titulo;$textinputvalue[tituloopticket]]
