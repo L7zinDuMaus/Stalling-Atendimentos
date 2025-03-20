a0a402482ce15caf36059b080c4c1c2925774f5e
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-20 00:31:57 -0300

    update remopcao_ticketV2.js

--- Comandos/Tickets/config/remopcao_ticketV2.js
+++ Comandos/Tickets/config/remopcao_ticketV2.js
@@ -11,62 +11,6 @@ module.exports = {
  $let[menu2;$get[menu]]
  $let[menu;{actionRow:{selectMenu:rmopcao_ticket:Escolha uma Opção para Removê-la.:1:1:false:$if[$getguildvar[emoji1]!=n;{stringInput:$getguildvar[titulo1]:opcao1:$getguildvar[desc1]::$getguildvar[emoji1]}]$if[$getguildvar[emoji2]!=n;:{stringInput:$getguildvar[titulo2]:opcao2:$getguildvar[desc2]::$getguildvar[emoji2]}]$if[$getguildvar[emoji3]!=n;:{stringInput:$getguildvar[titulo3]:opcao3:$getguildvar[desc3]::$getguildvar[emoji3]}]$if[$getguildvar[emoji4]!=n;:{stringInput:$getguildvar[titulo4]:opcao4:$getguildvar[desc4]::$getguildvar[emoji4]}]$if[$getguildvar[emoji5]!=n;:{stringInput:$getguildvar[titulo5]:opcao5:$getguildvar[desc5]::$getguildvar[emoji5]}]}}]
  
-$resetGuildVar[titulo5]
-$resetGuildVar[desc5]
-$resetGuildVar[emoji5] 
- 
-$switch[$getSelectMenuValues[all;,];
-  {case:opcao1}
-    $setGuildVar[emoji5;$getGuildVar[emoji4]]
-    $setGuildVar[emoji4;$getGuildVar[emoji3]]
-    $setGuildVar[emoji3;$getGuildVar[emoji2]]
-    $setGuildVar[emoji2;$getGuildVar[emoji1]]
-    
-    $setGuildVar[desc5;$getGuildVar[desc4]]
-    $setGuildVar[desc4;$getGuildVar[desc3]]
-    $setGuildVar[desc3;$getGuildVar[desc2]]
-    $setGuildVar[desc2;$getGuildVar[desc1]]
-    
-    $setGuildVar[titulo5;$getGuildVar[titulo4]]
-    $setGuildVar[titulo4;$getGuildVar[titulo3]]
-    $setGuildVar[titulo3;$getGuildVar[titulo2]]
-    $setGuildVar[titulo2;$getGuildVar[titulo1]]
-    
-
-  {case:opcao2}
-    $setGuildVar[emoji5;$getGuildVar[emoji4]]
-    $setGuildVar[emoji4;$getGuildVar[emoji3]]
-    $setGuildVar[emoji3;$getGuildVar[emoji2]]
-
-    $setGuildVar[desc5;$getGuildVar[desc4]]
-    $setGuildVar[desc4;$getGuildVar[desc3]]
-    $setGuildVar[desc3;$getGuildVar[desc2]]
-
-    $setGuildVar[titulo5;$getGuildVar[titulo4]]
-    $setGuildVar[titulo4;$getGuildVar[titulo3]]
-    $setGuildVar[titulo3;$getGuildVar[titulo2]]
-
-  {case:opcao3}
-    $setGuildVar[emoji5;$getGuildVar[emoji4]]
-    $setGuildVar[emoji4;$getGuildVar[emoji3]]
-
-    $setGuildVar[desc5;$getGuildVar[desc4]]
-    $setGuildVar[desc4;$getGuildVar[desc3]]
-
-    $setGuildVar[titulo5;$getGuildVar[titulo4]]
-    $setGuildVar[titulo4;$getGuildVar[titulo3]]
-
-  {case:opcao4}
-    $setGuildVar[emoji5;$getGuildVar[emoji4]]
-
-    $setGuildVar[desc5;$getGuildVar[desc4]]
-
-    $setGuildVar[titulo5;$getGuildVar[titulo4]]
-
-  {case:opcao5}
-    // Caso precise de alguma ação específica para essa opção, pode adicionar aqui.
-]
- 
  $resetguildvar[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$nonescape[$getSelectMenuValues[all;,]];opcao1;titulo1];opcao2;titulo2];opcao3;titulo3];opcao4;titulo4];opcao5;titulo5]]
  $resetguildvar[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$nonescape[$getSelectMenuValues[all;,]];opcao1;emoji1];opcao2;emoji2];opcao3;emoji3];opcao4;emoji4];opcao5;emoji5]]
  $resetguildvar[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$nonescape[$getSelectMenuValues[all;,]];opcao1;desc1];opcao2;desc2];opcao3;desc3];opcao4;desc4];opcao5;desc5]]
