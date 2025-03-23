9fb0d7d0e261e4d110fcf76f5f34561b8d62bf29
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-23 18:44:17 -0300

    update

--- index.js
+++ index.js
@@ -65,6 +65,32 @@ client.awaitedCommand({
 
 client.loadCommands("./Comandos"); // Carrega os comandos da pasta "comandos"
 
+client.functionManager.createFunction({
+  name: "$transcript",
+  type: "djs",
+  code: async d => {
+    const discordTranscripts = require("discord-html-transcripts");
+    const data = d.util.aoiFunc(d);
+    const [channel = d.message.channel.id, logchannel = d.message.channel.id] = data.inside.splits;
+    let channelid = await d.util.getChannel(d, channel);
+    let loggingid = await d.util.getChannel(d, logchannel);
+    const attachment = await discordTranscripts.createTranscript(channelid, {
+      filename: "transcript.html",
+      saveImages: true,
+      poweredBy: false,
+    });
+
+    const f = await loggingid.send({
+      files: [attachment],
+    });
+
+    data.result = f;
+    return {
+      code: d.util.setCode(data),
+    };
+  }
+});
+
 // Iniciar o bot
 client.login();
 
