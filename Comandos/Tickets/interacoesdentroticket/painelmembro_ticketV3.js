73e065349b59b5e3fe79354bb9a8b852cc5aed1d
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-25 15:18:21 -0300

    update

--- Comandos/Tickets/interacoesdentroticket/painelmembro_ticketV3.js
+++ Comandos/Tickets/interacoesdentroticket/painelmembro_ticketV3.js
@@ -3,6 +3,17 @@ module.exports = {
   type: "interaction",
   prototype: "button",
   code: `
+  $channelsendmessage[$getGuildVar[registros_ticket];{newEmbed: {author:$guildname | Atendimentos}{color:$getGuildVar[color_ticket]}{footer:$guildname | Todos os Direitos Reservados.}{thumbnail:$guildicon}
+{title:Nova Notificação de Ticket.}
+{field:$customemoji[user] | Quem Notificou:<@$getChannelVar[solicitou_ticket]> ($username[$getChannelVar[solicitou_ticket]] - $getChannelVar[solicitou_ticket])}
+{field:$customemoji[assumir] | Quem foi Notificado:<@$getChannelVar[assumido_ticket]> ($username[$getChannelVar[assumido_ticket]] - $getChannelVar[assumido_ticket])}
+{field:$customemoji[registros] | Categoria:$getChannelVar[categoria_ticket] ($getChannelVar[emoji_ticket])}
+{field:$customemoji[canal] | Canal do Ticket:<#$channelid>}
+{field:$customemoji[id] | ID do Ticket:$channelid}
+}]
+
+$onlyif[$getGuildVar[registros_ticket]!=;]
+  
   
   $senddm[**Olá <@$getChannelVar[assumido_ticket]>! O usuário <@$authorid> ($username) está lhe notificando em um Ticket de Atendimento.**
 {actionRow: {button:Ir para Ticket de Atendimento:link:$messageurl[$sendmessage[**<@$authorid>, o Staff <@$getChannelVar[assumido_ticket]> foi notificado com sucesso.**;true]]::🛩}};$getChannelVar[assumido_ticket]]
--- Comandos/Tickets/interacoesdentroticket/painelstaff_ticketV3.js
+++ Comandos/Tickets/interacoesdentroticket/painelstaff_ticketV3.js
@@ -3,6 +3,16 @@ module.exports = {
   type: "interaction",
   prototype: "button",
   code: `
+  $channelsendmessage[$getGuildVar[registros_ticket];{newEmbed: {author:$guildname | Atendimentos}{color:$getGuildVar[color_ticket]}{footer:$guildname | Todos os Direitos Reservados.}{thumbnail:$guildicon}
+{title:Nova Notificação de Ticket.}
+{field:$customemoji[assumir] | Quem Notificou:<@$authorid> ($username - $authorid)}
+{field:$customemoji[user] | Quem foi Notificado:<@$getChannelVar[solicitou_ticket]> ($username[$getChannelVar[solicitou_ticket]] - $getChannelVar[solicitou_ticket])}
+{field:$customemoji[registros] | Categoria:$getChannelVar[categoria_ticket] ($getChannelVar[emoji_ticket])}
+{field:$customemoji[canal] | Canal do Ticket:<#$channelid>}
+{field:$customemoji[id] | ID do Ticket:$channelid}
+}]
+  
+  $onlyif[$getGuildVar[registros_ticket]!=;]
   
   $senddm[**Olá <@$getChannelVar[solicitou_ticket]>! O Staff <@$authorid> ($username) está lhe notificando no seu Ticket de Atendimento. Caso não compareça, seu Ticket pode ser \`Fechado\`.**
 {actionRow: {button:Ir para Ticket de Atendimento:link:$messageurl[$sendmessage[**<@$authorid>, o Membro <@$getChannelVar[solicitou_ticket]> foi notificado com sucesso.**;true]]::🛩}};$getChannelVar[solicitou_ticket]]
