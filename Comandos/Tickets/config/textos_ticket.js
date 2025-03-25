4f96505777dceb6c27c41dfe4c8412fde24385d5
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-25 11:27:45 -0300

    update textos_ticket.js

--- Comandos/Tickets/config/textos_ticket.js
+++ Comandos/Tickets/config/textos_ticket.js
@@ -7,11 +7,11 @@ module.exports = {
   {actionRow:
     {textInput:Qual o Título do Painel?:1:titulopainel:false:Escreva RESETAR para deixar padrão.:5:50:getGuildVar[titulo_ticket]}
   }{actionRow:
-    {textInput:Qual a Descricao do Painel?:1:descricaopainel:false:Escreva RESETAR para deixar padrão.:5:1000:getGuildVar[descricao_ticket]}
+    {textInput:Qual a Descricao do Painel?:2:descricaopainel:false:Escreva RESETAR para deixar padrão.:5:1000:getGuildVar[descricao_ticket]}
   }{actionRow:
     {textInput:Qual o Título do Painel de Boas-Vindas?:1:titulobbpainel:false:Escreva RESETAR para deixar padrão.:5:50:getGuildVar[titulo_b_ticket]}
   }{actionRow:
-    {textInput:Qual a Descricao do Painel de Boas Vindas?:1:descricaobbpainel:false:Escreva RESETAR para deixar padrão.:5:1000:getGuildVar[descricao_b_ticket]}
+    {textInput:Qual a Descricao do Painel de Boas Vindas?:2:descricaobbpainel:false:Escreva RESETAR para deixar padrão.:5:1000:getGuildVar[descricao_b_ticket]}
   }]
   `
 }
