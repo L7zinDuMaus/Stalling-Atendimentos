5202ff23ea3d9882be5e7ab0573fc3e8a3e98544
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-26 00:02:12 -0300

    update

--- index.js
+++ index.js
@@ -72,6 +72,33 @@ transcripting, i think..`
 })
 
 client.functionManager.createFunction({
+  name: "$transcript",
+  type: "djs",
+  code: async d => {
+    const discordTranscripts = require("discord-html-transcripts");
+    const data = d.util.aoiFunc(d);
+    const [channel = d.message.channel.id, logchannel = d.message.channel.id] = data.inside.splits;
+    let channelid = await d.util.getChannel(d, channel);
+    let loggingid = await d.util.getChannel(d, logchannel);
+    const attachment = await discordTranscripts.createTranscript(channelid, {
+      filename: "$channelname.html",
+      saveImages: true,
+      poweredBy: false,
+      footerText: "{number} Mensagens Transcritas.",
+    });
+
+    const f = await loggingid.send({
+      files: [attachment],
+    });
+
+    data.result = f;
+    return {
+      code: d.util.setCode(data),
+    };
+  }
+});
+
+client.functionManager.createFunction({
     name: "$transcriptDm",
     type: "djs",
     code: async d => {
