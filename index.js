4b2efdab8d5071c66d88ea66fede2971a1b9b4de
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-27 21:58:23 -0300

    update index.js

--- index.js
+++ index.js
@@ -52,18 +52,19 @@ client.command({
 });
 
 client.command({
-    name:"$alwaysExecute",
-    $if:"old",
-    code:`
+    name: "$alwaysExecute",
+    $if: "old",
+    code: `
 $if[$channelID==1354978265764462702]
-$jsonRequest[https://api.openai.com/v1/chat/completions;
+$let[resposta;$jsonRequest[https://api.openai.com/v1/chat/completions;
     {"model":"gpt-4","messages":[{"role":"user","content":"$message"}]};
     "choices.0.message.content";
-    {"Authorization":"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","Content-Type":"application/json"}]
-$reply[$messageID;false]
+    {"Authorization":"Bearer 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","Content-Type":"application/json"}]]
+
+$reply[$messageID;$get[resposta];false]
 $clientTyping
 $endif
-`})
+`});
  
 client.loadCommands("./Comandos"); // Carrega os comandos da pasta "comandos"
 
