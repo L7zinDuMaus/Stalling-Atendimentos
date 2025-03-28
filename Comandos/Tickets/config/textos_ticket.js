53fe586440217ac9a3012249c4eb0220878af159
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-27 21:01:12 -0300

    update textos_ticket.js

--- Comandos/Tickets/config/textos_ticket.js
+++ Comandos/Tickets/config/textos_ticket.js
@@ -5,15 +5,15 @@ module.exports = {
   code: `
   $interactionModal[Configuração de Textos;configurartextosticket;
   {actionRow:
-    {textInput:Qual o Título do Painel?:1:titulopainel:false:Escreva RESETAR para deixar padrão.:5:50:$getGuildVar[titulo_ticket]}
+    {textInput:Qual o Título do Painel?:1:titulopainel:true:Escreva RESETAR para deixar padrão.:5:50:$getGuildVar[titulo_ticket]}
   }{actionRow:
-    {textInput:Qual a Descricao do Painel?:2:descricaopainel:false:Escreva RESETAR para deixar padrão.:5:1000:$getGuildVar[descricao_ticket]}
+    {textInput:Qual a Descricao do Painel?:2:descricaopainel:true:Escreva RESETAR para deixar padrão.:5:1000:$getGuildVar[descricao_ticket]}
   }{actionRow:
-    {textInput:Qual o Título do Painel de Boas-Vindas?:1:titulobbpainel:false:Escreva RESETAR para deixar padrão.:5:50:$getGuildVar[titulo_b_ticket]}
+    {textInput:Qual o Título do Painel de Boas-Vindas?:1:titulobbpainel:true:Escreva RESETAR para deixar padrão.:5:50:$getGuildVar[titulo_b_ticket]}
   }{actionRow:
-    {textInput:Qual a Descricao do Painel de Boas Vindas?:2:descricaobbpainel:false:Escreva RESETAR para deixar padrão.:5:1000:$getGuildVar[descricao_b_ticket]}
+    {textInput:Qual a Descricao do Painel de Boas Vindas?:2:descricaobbpainel:true:Escreva RESETAR para deixar padrão.:5:1000:$getGuildVar[descricao_b_ticket]}
   }{actionRow:
-    {textInput:Qual Banner Deseja Utilizar?:1:bannerpainel:true:Escreva RESETAR para deixar padrão.:5:400:$getGuildVar[banner_ticket]}
+    {textInput:Qual Banner Deseja Utilizar?:2:bannerpainel:true:Escreva RESETAR para deixar padrão.:5:400:$getGuildVar[banner_ticket]}
   }]
   `
 }
