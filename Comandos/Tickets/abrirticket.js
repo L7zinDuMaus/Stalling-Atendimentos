83d6a7dce667e41645c179c959dc0426da6aa315
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-27 21:14:57 -0300

    update abrirticket.js

--- Comandos/Tickets/abrirticket.js
+++ Comandos/Tickets/abrirticket.js
@@ -15,10 +15,11 @@ module.exports = {
   $setChannelVar[abertura_ticket;$truncate[$divide[$dateStamp;1000]];$get[idtopicoticket]]
   $setchannelvar[solicitou_ticket;$authorid;$get[idtopicoticket]]
   $interactionReply[O seu Ticket foi aberto com sucesso em $messageurl[$channelsendmessage[$get[idtopicoticket];<@$authorid>$if[$getGuildVar[cargo_ticket]!=; | <@&$getguildvar[cargo_ticket]>]. {newEmbed: {footer:$guildname | Todos os Direitos Reservados} {thumbnail:$guildicon}{image:$getGuildVar[banner_ticket]}{author:$guildname | Atendimentos}{title:$getguildvar[titulo_b_ticket]}{description:$replaceText[$replaceText[$replaceText[$replaceText[$getguildvar[descricao_b_ticket];|EMOJI|;$getChannelVar[emoji_ticket;$get[idtopicoticket]]];|ATENDENTE|;Ticket ainda não Assumido];|USUARIO|;<@$authorid>];|CATEGORIA|;$touppercase[$getChannelVar[categoria_ticket;$get[idtopicoticket]]]]}{color:$getGuildVar[color_ticket]}{footer:$getGuildVar[footer_ticket]}}{actionRow:
-  {button:Fechar Ticket:secondary:fecharticket::$customemoji[cadeado]}
-  {button:Assumir Ticket:secondary:assumir_ticket::️$customemoji[assumir]}
-  {button:Painel Membro:secondary:painelmembroticket::$customemoji[memberpanel]}
-  {button:Painel Staff:secondary:painelstaffticket::$customemoji[staffpanel]}};true];$get[idtopicoticket]];;true]
+  {button:Fechar Ticket:secondary:fecharticket::}
+  {button:Assumir Ticket:secondary:assumir_ticket::}
+  {button:Painel Membro:secondary:painelmembroticket::}
+  {button:Painel Staff:secondary:painelstaffticket::}};true];$get[idtopicoticket]];;true]
+  
   $setChannelVar[emoji_ticket;$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$nonescape[$getSelectMenuValues[all;,]];opcao1;$getGuildVar[emoji1]];opcao2;$getGuildVar[emoji2]];opcao3;$getGuildVar[emoji3]];opcao4;$getGuildVar[emoji4]];opcao5;$getGuildVar[emoji5]];$get[idtopicoticket]]
   $setChannelVar[categoria_ticket;$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$nonescape[$getSelectMenuValues[all;,]];opcao1;$getGuildVar[titulo1]];opcao2;$getGuildVar[titulo2]];opcao3;$getGuildVar[titulo3]];opcao4;$getGuildVar[titulo4]];opcao5;$getGuildVar[titulo5]];$get[idtopicoticket]]
   
