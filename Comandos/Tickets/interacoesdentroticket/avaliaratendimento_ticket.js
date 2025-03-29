20798eb2397f61e84b5cc7633c42ccf91de56dd1
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-29 10:38:19 -0300

    update

--- Comandos/Tickets/interacoesdentroticket/avaliaratendimento_ticket.js
+++ Comandos/Tickets/interacoesdentroticket/avaliaratendimento_ticket.js
@@ -3,7 +3,7 @@ module.exports = {
   type: "interaction",
   prototype: "button",
   code: `
-  $interactionModal[Renomear Ticket de Atendimento;avaliacaoaten_ticket;{actionRow: {textInput:Quantas Estrelas?:1:estrelasaten_ticket:true:Insira entre 1 a 5 estrelas.:1:1}}{actionRow:{textInput:Avaliação Escrita?:1:escritaaten_ticket:true:Insira sua Avaliação.:5:50}}]
+  $interactionModal[Avaliação de Atendimento;avaliacaoaten_ticket;{actionRow: {textInput:Quantas Estrelas?:1:estrelasaten_ticket:true:Insira entre 1 a 5 estrelas.:1:1}}{actionRow:{textInput:Avaliação Escrita?:1:escritaaten_ticket:true:Insira sua Avaliação.:5:50}}]
   $suppressErrors
  
   
