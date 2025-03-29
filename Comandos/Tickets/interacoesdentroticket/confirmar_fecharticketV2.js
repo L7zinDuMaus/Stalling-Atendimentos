99d9422614573e883c33fc80b009a4a2baec14d3
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-29 13:16:19 -0300

    update

---
+++ Comandos/Tickets/interacoesdentroticket/confirmar_fecharticketV2.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "confirmar_fecharticket",
+  type: "interaction",
+  prototype: "modal",
+  code: `
+  $deletechannel[$channelid]
+  $wait[11s]
+  $onlyif[$getGuildVar[registros_ticket]==;]
+  `}
