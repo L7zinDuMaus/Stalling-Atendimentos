823b0ad017f73b88ecd4b9b6d3c05c51fe1e7af5
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-28 22:42:58 -0300

    update painelstaff_ticketV4.js

--- Comandos/Tickets/interacoesdentroticket/painelstaff_ticketV4.js
+++ Comandos/Tickets/interacoesdentroticket/painelstaff_ticketV4.js
@@ -15,13 +15,17 @@ module.exports = [{
 {title:Novo Pagamento Solicitado.}
 {field:$customemoji[user] | Quem foi Cobrado:<@$getChannelVar[solicitou_ticket]> ($username[$getChannelVar[solicitou_ticket]] - $getChannelVar[solicitou_ticket])}
 {field:$customemoji[assumir] | Quem Cobrou:<@$authorid> ($username - $authorid)}
-{field:$customEmoji[registros] | Chave PIX:$getChannelVar[chavepix]}
-{field:$customemoji[star] | Valor Cobrado:$getChannelVar[valorpix]}
+{field:$customEmoji[registros] | Chave PIX:**$getChannelVar[chavepix]**}
+{field:$customemoji[star] | Valor Cobrado:**\`R\$ $getChannelVar[valorpix]\`**}
 {field:$customemoji[registros] | Categoria:$getChannelVar[categoria_ticket] ($getChannelVar[emoji_ticket])}
 {field:$customemoji[canal] | Canal do Ticket:<#$channelid>}
 {field:$customemoji[id] | ID do Ticket:$channelid}
 }]
 $onlyif[$getGuildVar[registros_ticket]!=;]
+
+$senddm[**Olá <@$getChannelVar[solicitou_ticket]>! O Staff <@$authorid> ($username) acabou de fazer uma cobrança de \`R\$ $getChannelVar[valorpix]\` para você.**
+{actionRow: {button:Ir para Ticket de Atendimento:link:$messageurl[$sendmessage[**<@$authorid>, o Membro <@$getChannelVar[solicitou_ticket]> foi notificado desta Cobrança com sucesso.**;true]]::🛩}};$getChannelVar[solicitou_ticket]]
+
   $interactionedit[$criarpagamento[$textinputvalue[chavepix_ticket];$textinputvalue[valorpix_ticket];$guildname] {actionRow: {button:Notificar Membro:primary:notificarmembro_ticket::📨}{button:Renomear Ticket:success:renomear_ticket:true:🖌}{button:Gerar Pagamento:secondary:gerar_pagamento:true:💵}};;true]
   $setChannelVar[chavepix;$textinputvalue[chavepix_ticket]]
   $setChannelVar[valorpix;$textinputvalue[valorpix_ticket]]
