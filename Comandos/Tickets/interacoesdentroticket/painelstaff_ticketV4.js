32d06c28b7224c23aea5c3f634a5c4732e2aca4d
L7zinDuMaus <davidbreno810@gmail.com>
2025-03-28 22:23:47 -0300

    update painelstaff_ticketV4.js

--- Comandos/Tickets/interacoesdentroticket/painelstaff_ticketV4.js
+++ Comandos/Tickets/interacoesdentroticket/painelstaff_ticketV4.js
@@ -5,16 +5,28 @@ module.exports = [{
   code: `
   $interactionModal[Gerar Pagamentos;gerarpagamento_ticket; {actionRow: {textInput:Qual o Valor do Pagamento?:1:valorpix_ticket:true:Ex. 4.99:1:5}} {actionRow: {textInput:Qual a Chave PIX do Pagamento?:1:chavepix_ticket:true:Ex. 4.99:5:400:$getGuildVar[chavepix]}}
   
-  
   `
 }, {
   name: "gerarpagamento_ticket",
   type: "interaction",
   prototype: "modal",
   code: `
-  $interactionreply[$criarpagamento[$textinputvalue[chavepix_ticket];$textinputvalue[valorpix_ticket];$guildname];;true]
+  $channelsendmessage[$getGuildVar[registros_ticket];{newEmbed: {author:$guildname | Atendimentos}{color:$getGuildVar[color_ticket]}{footer:$guildname | Todos os Direitos Reservados.}{thumbnail:$guildicon}
+{title:Novo Pagamento Solicitado.}
+{field:$customemoji[user] | Quem foi Cobrado:<@$getChannelVar[solicitou_ticket]> ($username[$getChannelVar[solicitou_ticket]] - $getChannelVar[solicitou_ticket])}
+{field:$customemoji[assumir] | Quem Cobrou:<@$authorid> ($username - $authorid)}
+{field:$customEmoji[configurar] | Chave PIX:$getChannelVar[chavepix]}
+{field:$customemoji[star] | Valor Cobrado:$getChannelVar[valorpix]}
+{field:$customemoji[registros] | Categoria:$getChannelVar[categoria_ticket] ($getChannelVar[emoji_ticket])}
+{field:$customemoji[canal] | Canal do Ticket:<#$channelid>}
+{field:$customemoji[id] | ID do Ticket:$channelid}
+}]
+
+$onlyif[$getGuildVar[registros_ticket]!=;]
+  $interactionupdate[$criarpagamento[$textinputvalue[chavepix_ticket];$textinputvalue[valorpix_ticket];$guildname];;true]
   $setChannelVar[chavepix;$textinputvalue[chavepix_ticket]]
   $setChannelVar[valorpix;$textinputvalue[valorpix_ticket]]
+  $onlyPerms[administrator;$interactionReply[**❌️ | Apenas Administradores podem usar esta função.**;;true]
   
   `
 }, {
